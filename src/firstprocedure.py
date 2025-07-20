f9d9882ca451e0d5f60ceb9f1ac86890778dfead
MKarthik1729 <karthik.m20@iiits.in>
2025-07-20 15:48:26 +0530

    created a first pipepline without params

--- src/firstprocedure.py
+++ src/firstprocedure.py
@@ -16,7 +16,7 @@ def firstprocedure(a,b,c):
 if __name__ == "__main__":
     # print(firstprocedure(1,2))
     # read the csv file
-    df = pd.read_csv("data/values.csv")
+    df = pd.read_csv("data1/values.csv")
     print(df)
     # add a column to the csv file
     # df["sum"] = df["a"] + df["b"]
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     # df["sum"] = data
     total={"sum":[data[0]]}
     df = pd.DataFrame(total)
-    df.to_csv("data/first_procedure_output.csv",index=False)
+    df.to_csv("data1/first_procedure_output.csv",index=False)
 
     # create a csv file with the sum of the values
 
--- src/secondprocedure.py
+++ src/secondprocedure.py
@@ -7,14 +7,15 @@ def secondprocedure(a,b):
 
 if __name__ == "__main__":
     # read the csv file
-    df = pd.read_csv("data/first_procedure_output.csv")
+    df = pd.read_csv("data1/first_procedure_output.csv")
     print(df)
     # add a column to the csv file
     print(df["sum"][0])
-    multiplier = 20
+    adder = 5
+    multiplier = 20 + adder
     data = secondprocedure(df["sum"][0],multiplier)
     print(data)
     # save the csv file
     total={"multiplied":[data]}
     df = pd.DataFrame(total)
-    df.to_csv("data/second_procedure_output.csv",index=False)
+    df.to_csv("data1/second_procedure_output.csv",index=False)
